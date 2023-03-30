81d9bf2cbb65e57f626b6ac8edb42533d5fefa41
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-03-30 16:14:25 +0200

    added context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import { useState } from "react";
 import styles from "./App.module.scss";
+
+import { useContext, useState } from "react";
 
 import Content from "./pages/Content";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import { ApiContext } from "./context/ApiContext";
 
 function App() {
   const [actualLocation, setActualLocation] = useState("HOME");
+  const USER_API = useContext(ApiContext);
   const stateUserLogged = localStorage.getItem("Logged");
   console.log(actualLocation);
+  //Make sure that a logged user can go to the Login and Register content
   if (stateUserLogged && actualLocation === "LOGIN") {
+    setActualLocation("PROFIL");
+  }
+  if (stateUserLogged && actualLocation === "REGISTER") {
     setActualLocation("PROFIL");
   }
   //Function that get when an header button is click and change the variable actualLocation
@@ -61,9 +68,9 @@ function App() {
         return "error";
     }
   }
-
+  //Function that handle all the backend call
   async function FetchPost(action, JsonValue) {
-    const response = await fetch(`http://localhost:8000/${action}`, {
+    const response = await fetch(`${USER_API}/${action}`, {
       method: "POST",
       body: JsonValue,
       headers: {
@@ -75,7 +82,7 @@ function App() {
 
     return responseFromBackEnd;
   }
-
+  //Function interm
   async function handleFetch(action, value) {
     const JsonValue = JSON.stringify(value);
     switch (action) {
