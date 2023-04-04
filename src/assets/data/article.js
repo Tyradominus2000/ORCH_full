9267a9881fc7e2c18e8240eb9832f2c0e93a464b
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-04-04 14:17:07 +0200

    feat: added a test data file

---
+++ src/assets/data/article.js
@@ -0,0 +1,39 @@
+export const article = [
+    {
+      title: "RX 6750XT",
+      image: "assets/images/rx6750xt.png",
+    },
+    {
+      title: "i7-10700k",
+      image: "assets/images/i710700k.png",
+    },
+    {
+      title: "RTX 3060",
+      image: "assets/images/rtx3060.png",
+    },
+    {
+      title: "Ryzen 7500 series",
+      image: "assets/images/amdryzen75000series.png",
+    },
+    {
+      title: "Noctua NH-D14",
+      image: "assets/images/NoctuaNH-D14.png",
+    },
+    {
+      title: "I3 13500",
+      image: "assets/images/i313500.png",
+    },
+    {
+      title: "Corsair Vengeance DDR4",
+      image: "assets/images/CorsairVengeanceDDR4.webp",
+    },
+    {
+      title: "RTX 4090",
+      image: "assets/images/RTX4090.png",
+    },
+    {
+      title: "Intel ARC a770",
+      image: "assets/images/arca770.png",
+    },
+  ];
+  
