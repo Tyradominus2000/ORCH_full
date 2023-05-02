e88c3f1c23078fc53c6114d3ce382171cbb31266
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-05-02 10:45:17 +0200

    fix : change logic in loader

--- src/loader/ProfilLoader.js
+++ src/loader/ProfilLoader.js
@@ -2,16 +2,29 @@ import { redirect } from "react-router-dom";
 const { API_BackendURL } = require("../context/ApiURL");
 
 export async function ProfilLoader() {
-  const responseAuth = await fetch(API_BackendURL + "/Auth", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  });
-  if (await responseAuth.json() === false) {
-    return redirect("/content/login");
+  const token = getCookie("token");
+  function getCookie(cookieName) {
+    let cookie = {};
+    document.cookie.split(";").forEach(function (el) {
+      let [key, value] = el.split("=");
+      cookie[key.trim()] = value;
+    });
+    return cookie[cookieName];
+  }
+  if (token) {
+    const responseAuth = await fetch(API_BackendURL + "/Auth", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    if ((await responseAuth.json()) === false) {
+      return redirect("/content/login");
+    } else {
+      return true;
+    }
   } else {
-    return true;
+    return redirect("/content/login");
   }
 }
--- src/loader/RegisterLoader.js
+++ src/loader/RegisterLoader.js
@@ -2,15 +2,28 @@ import { redirect } from "react-router-dom";
 const { API_BackendURL } = require("../context/ApiURL");
 
 export async function RegisterLoader() {
-  const responseAuth = await fetch(API_BackendURL + "/Auth", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  });
-  if (await responseAuth.json() !== false) {
-    return redirect("/content/profil");
+  const token = getCookie("token");
+  function getCookie(cookieName) {
+    let cookie = {};
+    document.cookie.split(";").forEach(function (el) {
+      let [key, value] = el.split("=");
+      cookie[key.trim()] = value;
+    });
+    return cookie[cookieName];
+  }
+  if (token) {
+    const responseAuth = await fetch(API_BackendURL + "/Auth", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    if ((await responseAuth.json()) !== false) {
+      return redirect("/content/profil");
+    } else {
+      return true;
+    }
   } else {
     return true;
   }
